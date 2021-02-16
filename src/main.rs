8b53f41c40402da187b37e3f1d9d6ad563301d27
Siemen Kraayenbrink <siemenkraayenbrink@Siemens-MacBook-Pro.local>
2021-02-16 12:52:24 +0100

    blue screen boilerplate

---
+++ src/main.rs
@@ -0,0 +1,161 @@
+use winit::{
+    event::*,
+    event_loop::{EventLoop, ControlFlow},
+    window::{Window, WindowBuilder}
+};
+
+struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    sc_desc: wgpu::SwapChainDescriptor,
+    swap_chain: wgpu::SwapChain,
+    size: winit::dpi::PhysicalSize<u32>,
+}
+
+impl State {
+    async fn new (window: &Window) -> Self {
+        let size = window.inner_size();
+        // Handle to gpu
+        let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
+        let surface = unsafe { instance.create_surface(window) };
+        let adapter = instance.request_adapter(
+            &wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::Default,
+                compatible_surface: Some(&surface),
+            },
+        ).await.unwrap();
+
+        // Use the adapter to quest device and queue.
+        //  You can view available features through device.features()
+        let (device, queue) = adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                features: wgpu::Features::empty(),
+                limits: wgpu::Limits::default(),
+                shader_validation: true,
+            },
+            None,
+        ).await.unwrap();
+
+        let sc_desc = wgpu::SwapChainDescriptor {
+            usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+
+        let swap_chain = device.create_swap_chain(&surface, &sc_desc);
+
+        Self { surface, device, queue, sc_desc, swap_chain, size }
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        self.size = new_size;
+        self.sc_desc.width = new_size.width;
+        self.sc_desc.height = new_size.height;
+        self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        false
+    }
+
+    fn update(&mut self) {
+        //todo!()
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SwapChainError> {
+        // Sort of  ogl framebuffer I guess?
+        let frame = self.swap_chain
+            .get_current_frame()?
+            .output;
+        
+        // Encoders creates a commandbuffer.
+        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        });
+        let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            color_attachments: &[
+                wgpu::RenderPassColorAttachmentDescriptor {
+                    attachment: &frame.view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1, g: 0.2, b: 0.3, a: 1.0,
+                        }),
+                        store: true,
+                    }
+                }
+            ],
+            depth_stencil_attachment: None,
+        });
+
+        drop(_render_pass);
+        self.queue.submit(std::iter::once(encoder.finish()));
+
+        Ok(())
+    }
+}
+
+fn main() {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new() .build(&event_loop) .unwrap();
+
+    // fn main() cannot be async, so block the main thread until future complete.
+    use futures::executor::block_on;
+    let mut state = block_on(State::new(&window));
+
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => if !state.input(event) {
+                match event {
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    WindowEvent::KeyboardInput {
+                        input, ..
+                    } => {
+                        match input {
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            } => *control_flow = ControlFlow::Exit,
+                            // Add more keyhandling here I guess.
+                            _ => {}
+                        }
+                    }
+                    _ => {}
+                    WindowEvent::Resized(physical_size) => {
+                        state.resize(*physical_size);
+                    }
+                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                        state.resize(**new_inner_size);
+                    }
+                    _ => {}
+                }
+            }
+            Event::RedrawRequested(_) => {
+                state.update();
+                match state.render() {
+                    // All good.
+                    Ok(_) => {}
+                    // Recreate the sc if it is lost.
+                    Err(wgpu::SwapChainError::Lost) => state.resize(state.size),
+                    // Out of mem, just exit the program.
+                    Err(wgpu::SwapChainError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                    // All other errrors should be resolved by the next frame.
+                    Err(e) => eprintln!("{:?}", e),
+                }
+            }
+            Event::MainEventsCleared => {
+                // RedrawRequested is only triggered once, we need to do so manually in the loop:
+                window.request_redraw();
+            }
+            _ => {}
+        }
+    });
+}
